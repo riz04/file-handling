eeb4625a327a12d61d924047246cf2c0fd57d673
rizulgoyal <goyalrizul1997@gmail.com>
2019-04-04 20:59:37 +0530

    initial notes on file handling in python

---
+++ write-to-file.py
@@ -0,0 +1,10 @@
+# This file contains code to learn how to write data to a file using python
+# This is also known as storing data in flat files
+# The type of files can be .csv, .json or even .txt
+# Other ways to store data is to store data inside a database
+
+
+# For now we are focusing on storing data in files
+
+# To read from/write to a file in Python first we have to open it using the in-built open() function
+# Never forget to close the file, else it will stay in the memory(RAM)
