b4876bfffc8324415a5e16ec5d8a878435d89fcb
rizulgoyal <goyalrizul1997@gmail.com>
2019-04-05 23:15:13 +0530

    appending in a file

--- infinite-loop.py
+++ infinite-loop.py
@@ -7,7 +7,17 @@
 
 while True:
     name = input("Enter your name: ")
+    mobile_no = int(input("Enter your number: "))
+
+    # now we need to write the data to file
+
+    # opening the file
+    phone_book = open("phonebook.csv", "a+")
+
+    # appending in the file
+    phone_book.write("\n%s, %d" % (name, mobile_no))
+
     should_continue = input("Dou you want to continue? (Y/N)")
-    if (should_continue == "N"):
+    if (should_continue == "N" or should_continue == "n"):
         break
 print("You opt to exit the program")
